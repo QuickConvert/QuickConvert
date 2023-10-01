5484f2b5f4a288efcc6f628a1e75cd3efa8a37a5
N3v1 <n3v1010@gmail.com>
2023-10-01 21:28:30 +0200

    Initial Commit

---
+++ QuickConvert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickConvert
+//
+//  Created by Nevio Hirani on 01.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickConvert/QuickConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickConvertApp.swift
+//  QuickConvert
+//
+//  Created by Nevio Hirani on 01.10.23.
+//
+
+import SwiftUI
+
+@main
+struct QuickConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
