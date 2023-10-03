1c3f8f310a1ba49022be0f80fc29e36adaad1dfa
N3v1 <n3v1010@gmail.com>
2023-10-03 06:55:33 +0200

    creating a search view model

---
+++ QuickConvert/Core/Search/View/SearchView.swift
@@ -0,0 +1,29 @@
+//
+//  SearchView.swift
+//  QuickConvert
+//
+//  Created by Nevio Hirani on 02.10.23.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    @State private var searchText = ""
+    @StateObject var viewModel = SearchViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            LazyVStack {
+                
+            // Search func
+            //.searchable(text: $searchText, prompt: "Search")
+            }
+            .navigationTitle("Search")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    SearchView()
+}
---
+++ QuickConvert/Core/Search/ViewModel/SearchViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SearchViewModel.swift
+//  QuickConvert
+//
+//  Created by Nevio Hirani on 03.10.23.
+//
+
+import Foundation
+
+class SearchViewModel: ObservableObject {
+    
+}
