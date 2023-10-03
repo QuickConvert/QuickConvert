38be492a82e388080fffaff0e24868c9e67b55e5
N3v1 <n3v1010@gmail.com>
2023-10-03 07:03:37 +0200

    creating navigation stacks

--- QuickConvert/Core/Favourite/FavouriteView.swift
+++ QuickConvert/Core/Favourite/FavouriteView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct FavouriteView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("Hello,World!")
+                Image(systemName: "glob")
+                    .foregroundColor(.blue)
+            }
+            .navigationTitle("Favourite")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
