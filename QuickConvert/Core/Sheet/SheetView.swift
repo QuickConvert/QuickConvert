1dac7e006d91a3cb36c6d49c30a90ab24b55e20a
N3v1 <n3v1010@gmail.com>
2023-10-04 08:25:25 +0200

    create some file and change TabView

---
+++ QuickConvert/Core/Sheet/SheetView.swift
@@ -0,0 +1,18 @@
+//
+//  SheetView.swift
+//  QuickConvert
+//
+//  Created by Nevio Hirani on 04.10.23.
+//
+
+import SwiftUI
+
+struct SheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetView()
+}
--- QuickConvert/Core/TabBar/TabView.swift
+++ QuickConvert/Core/TabBar/TabView.swift
@@ -9,34 +9,50 @@ import SwiftUI
 
 struct TabView: View {
     @State private var selectedTab = 0
+//    @State private var isSheetPresented = false
     
     var body: some View {
         Text("Hi")
-//        TabView(selection: $selectedTab) {
-//            CategoryView()
+        TabView(selection: $selectedTab) {
+            CategoryView()
+                .tabItem {
+                    Image(systemName: "bullet")
+                }
+                .onAppear { selectedTab = 0 }
+                .tag(0)
+            
+            FavouriteView()
+                .tabItem {
+                    Image(systemName: selectedTab == 1 ? "heart.fill" : "heart")
+//                    Image(systemName: selectedTab == 1 ? "star.fill" : "star")
+                        .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+                }
+                .onAppear { selectedTab = 1 }
+                .tag(1)
+            
+            SearchView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+                .onAppear { selectedTab = 2 }
+                .tag(2)
+            
+            // MARK: Sheet Tab
+//            Text("Tab 2")
 //                .tabItem {
-//                    Image(systemName: "bullet")
+//                    Image(systemName: "2.circle")
+//                    Text("Tab 2")
 //                }
-//                .onAppear { selectedTab = 0 }
-//                .tag(0)
-//            
-//            FavouriteView()
-//                .tabItem {
-//                    Image(systemName: selectedTab == 1 ? "heart.fill" : "heart") // maybe change icon to "star.fill" or "star"
-////                    Image(systemName: selectedTab == 1 ? "star.fill" : "star")
-//                        .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+//                .onTapGesture {
+//                    // When the second tab is tapped, present a sheet
+//                    isSheetPresented.toggle()
 //                }
-//                .onAppear { selectedTab = 1 }
-//                .tag(1)
-//            
-//            SearchView()
-//                .tabItem {
-//                    Image(systemName: "magnifyingglass")
+//                .sheet(isPresented: $isSheetPresented) {
+//                    // Content of the sheet
 //                }
-//                .onAppear { selectedTab = 2 }
-//                .tag(2)
-//        }
-//        .tint(.black) // changes the TabBar Buttons color to black
+            
+        }
+        .tint(.black) // changes the TabBar Buttons color to black
     }
 }
 
