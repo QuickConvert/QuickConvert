93afbf68a5a50a6d1a7e44fd7d27d591869b2283
N3v1 <n3v1010@gmail.com>
2023-10-05 19:46:04 +0200

    Update ConvertionFormView.swift

--- QuickConvert/Core/Convertion/View/ConvertionFormView.swift
+++ QuickConvert/Core/Convertion/View/ConvertionFormView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct ConvertionFormView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("Hello, World")
+                Image(systemName: "gear")
+                    .foregroundColor(.blue)
+            }
+            
+            .navigationTitle("Convertion")
+            .navigationBarTitleDisplayMode(.automatic)
+        }
     }
 }
 
