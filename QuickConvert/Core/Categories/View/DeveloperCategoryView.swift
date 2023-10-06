45c220baf44d3d270a40352221ad2870deeb59e3
N3v1 <n3v1010@gmail.com>
2023-10-06 18:54:03 +0200

    Create missing view
    Design needs some improvement - it's terrible

--- QuickConvert/Core/Categories/View/DeveloperCategoryView.swift
+++ QuickConvert/Core/Categories/View/DeveloperCategoryView.swift
@@ -24,24 +24,48 @@ struct DeveloperCategoryView: View {
                                 Text(option.title)
                                     .font(.subheadline)
                             }
-                            HStack {
-                                Image(systemName: "")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 24, height: 24)
-                                    .foregroundColor(.black)
-                                
-                                Text(option.description)
-                                    .frame(alignment: .leading)
-                                    .font(.caption)
-                                    .fontWeight(.semibold)
-                            }
+//                            HStack {
+//                                Image(systemName: "")
+//                                    .resizable()
+//                                    .scaledToFit()
+//                                    .frame(width: 24, height: 24)
+//                                    .foregroundColor(.black)
+//                                
+//                                Text(option.description)
+//                                    .frame(alignment: .leading)
+//                                    .font(.caption)
+//                                    .fontWeight(.semibold)
+//                            }
                         }
                     }
                 }
             }
             .navigationTitle("Developer")
             .navigationBarTitleDisplayMode(.automatic)
+            
+            VStack {
+                Image(systemName: "eye.trianglebadge.exclamationmark.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80, height: 60)
+                    .foregroundColor(.gray)
+                
+                Text("Some convertions are missing?")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text("Get in touch with us:")
+                    .font(.callout)
+                
+                // FIXME: Create Buttons instead of Text
+                Text("- [Email us](quick.convert.dev@gmail.com)")
+                    .font(.caption)
+                
+                Text("- Create an feature request on [GitHub](https://github.com/QuickConvert/QuickConvert/issues/new?assignees=&labels=enhancement&projects=&template=feature_request.yml&title=%E2%9C%A8+%3Cfeature+title%3E)")
+                    .font(.caption)
+                                
+            }
+            .fontWeight(.semibold)
+            .padding()
         }
     }
 }
